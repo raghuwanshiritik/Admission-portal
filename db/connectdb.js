8e14f3e03e8370a0c1ecb1e7dce23f4613238de7
ritikraghuwanshi <ritikraghuwanshi987@gmail.com>
2023-04-30 22:29:16 +0530

    ritik5

--- db/connectdb.js
+++ db/connectdb.js
@@ -1,9 +1,9 @@
 const mongoose  =require('mongoose')
 const url = "mongodb://127.0.0.1:27017/AdmissionPortal"
-
+const live_Url = "mongodb+srv://raghuvanshiritik537:raghuwanshi123@cluster0.8gz1tus.mongodb.net/admissionproject?retryWrites=true&w=majority"
 
 const connectdb =()=>{
-    return mongoose.connect(url)
+    return mongoose.connect(live_Url)
 
 
 
